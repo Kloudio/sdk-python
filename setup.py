3f3dfafd5e7eb81e936db85e89a64a51021a4cef
Jagannath Saragadam <jagannathsrs@gmail.com>
2020-07-08 12:26:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "kloud"
-VERSION = "0.09"
+VERSION = "0.092"
 # To install the library, run the following
 #
 # python setup.py install
